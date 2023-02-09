9ec6f8deaf01bddc77a17811118ad9573e599c56
kaspe4 <logdog062@gmail.com>
2023-02-09 15:16:04 +0300

    Now Hello a personalized.

--- main.cs
+++ main.cs
@@ -1,3 +1,5 @@
+Using System;
+
 namespace Git
 {
     class Git
@@ -5,6 +7,10 @@ namespace Git
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World!")
+            System.Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            System.Console.WriteLine(name.Lenght);
+            System.Console.WriteLine($"Hello {name}!");
         }
     }
 }
